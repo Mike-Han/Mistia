ac114456747478bb8f641833a56ddf994e64e79b
Mike Han <mike@alrightylabs.com>
2022-10-15 07:47:44 +0800

    feat: inital commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header dark elevated>
+      <q-toolbar>
+        <q-toolbar-title> Mistia Home </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row justify-center q-pt-lg">
+    <div class="col-4">
+      <q-card class="my-card" flat>
+        <img src="/src/assets/mistia-light.png" />
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
