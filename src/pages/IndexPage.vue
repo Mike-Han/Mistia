1035a46daf462f5ec324a0d37e75619e966ee211
Mike Han <mike@alrightylabs.com>
2022-10-15 08:11:35 +0800

    fix: fix logo link

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
   <div class="row justify-center q-pt-lg">
     <div class="col-4">
       <q-card class="my-card" flat>
-        <img src="/src/assets/mistia-light.png" />
+        <img src="../assets/mistia-light.png" />
       </q-card>
     </div>
   </div>
